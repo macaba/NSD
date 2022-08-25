7b9131817e8b0b6eacfb9679ae51614e1d0d2895
macaba <1031306+macaba@users.noreply.github.com>
2022-08-25 11:51:27 +0100

    Added white noise generator

--- source/NSD/NSD/Signals.cs
+++ source/NSD/NSD/Signals.cs
@@ -7,7 +7,7 @@ namespace NSD
         public static Memory<double> TenNanoVoltRmsTestSignal()
         {
             var sine = MathNet.Numerics.Generate.Sinusoidal(1000000, 50, 5.1, 1.41e-8);
-            for(int i = 0; i < sine.Length; i++)
+            for (int i = 0; i < sine.Length; i++)
             {
                 sine[i] = Math.Round(sine[i], 11);
             }
@@ -23,6 +23,17 @@ namespace NSD
                 sine[i] = sine[i] + noiseSource.ReadNextSample();
             }
             return sine;
+        }
+
+        public static Memory<double> WhiteNoise(int samples, double sampleRate, double volt)
+        {
+            var noiseSource = new MathNet.Filtering.DataSources.WhiteGaussianNoiseSource(0, 1);
+            Memory<double> data = new double[samples];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data.Span[i] = noiseSource.ReadNextSample() * volt * Math.Sqrt(sampleRate / 2);
+            }
+            return data;
         }
     }
 }
