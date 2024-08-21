4620422ab2a6d2a592cc9f76351d90326b5a07b5
macaba <1031306+macaba@users.noreply.github.com>
2024-08-21 13:17:42 +0100

    Fixes to workflow

--- build-scripts/NSD.UI (linux-x64).bat
+++ build-scripts/NSD.UI (linux-x64).bat
@@ -1,1 +1,2 @@
 dotnet publish ../source/NSD.UI -r linux-x64 -c Release --output ../builds/NSD.UI/linux-x64/
+ren ..\builds\NSD.UI\linux-x64\NSD.UI NSD-linux-x64
--- build-scripts/NSD.UI (win-x64).bat
+++ build-scripts/NSD.UI (win-x64).bat
@@ -1,1 +1,2 @@
 dotnet publish ../source/NSD.UI -r win-x64 -c Release --output ../builds/NSD.UI/win-x64/
+ren ..\builds\NSD.UI\win-x64\NSD.UI.exe NSD-win-x64.exe
